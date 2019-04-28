d2e918d5c1fa476dc6aae1115ea4a6833a27ff63
mixinan <mixinan@163.com>
2019-04-28 17:03:36 +0800

    add suanshu

--- routes/family.js
+++ routes/family.js
@@ -5,15 +5,17 @@ const path=require('path');
 var router=express.Router();
 
 //添加路由
+//默认首页
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'../public','family.html'));
+});
+
 //家庭成员列表
 router.get('/members',function(req,res){
 	res.send("家庭成员列表的网页");
 });
 
-//默认首页
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'../public','family.html'));
-});
+
 
 //导出路由器对象
 module.exports=router;
