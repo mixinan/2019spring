ba0243092a9b37100b76f715f21ba722c7a47651
mixinan <mixinan@163.com>
2019-05-03 18:42:54 +0800

    change dir

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const bodyParser=require('body-parser');
 server.listen(8080);
 
 //使用body-parser中间件，将post请求的数据格式化为对象
-server.use( bodyParser.urlencoded({
-    extended:false
-  }) );
+server.use( bodyParser.urlencoded({extended:false}) );
 
 server.use( express.static("public") );
+
 server.use('/work',workRouter);
 server.use('/family',familyRouter);
--- routes/family.js
+++ routes/family.js
@@ -15,7 +15,11 @@ router.get('/members',function(req,res){
 	res.send("家庭成员列表的网页");
 });
 
-
+router.post('/add', (req, res) => {
+    var obj=req.body;
+    console.log(obj);
+    res.send("姓名:"+obj.uname+"\n性别："+obj.gender);
+});
 
 //导出路由器对象
 module.exports=router;
